a34a3c896c48cf4502b80cc3244ade20d0ca3abf
varlock <varun4dinesh@gmail.com>
2025-02-16 11:58:29 -0500

    year

--- app/data/highlights.js
+++ app/data/highlights.js
@@ -1,5 +1,5 @@
 export const highlightList = [
-  { id: 1, text: 'Deep dive into the Web.3' },
+  { id: 1, text: 'Deep dive into the AI Solutions' },
   { id: 2, text: 'Learn Skateboard Tricks!' },
   { id: 3, text: 'Contribute to Open Source projects' }
 ];
