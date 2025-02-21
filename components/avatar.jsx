df5514eacf4ec233a77177378cd6ab2638264727
varlock <varun4dinesh@gmail.com>
2025-02-21 17:13:07 -0500

    pdesc

--- components/avatar.jsx
+++ components/avatar.jsx
@@ -37,7 +37,7 @@ const Avatar = () => {
       >
         <div className="wrap-avater">
           <Image
-            src="/images/icons/varlock_g.png"
+            src="/images/icons/varlock_h.png"
             alt="avatar"
             draggable="false"
             className="-mt-[20px] duration-500 group-hover:drop-shadow-[-1px_-1px_0px_#ec4899]"
